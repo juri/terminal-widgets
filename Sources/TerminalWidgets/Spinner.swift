70e179abd165308e7adfbac8be3efeccf9526e04
Juri Pakaste <juri@juripakaste.fi>
2025-10-29 18:32:13 +0200

    Add doc comments

--- Sources/TerminalWidgets/Spinner.swift
+++ Sources/TerminalWidgets/Spinner.swift
@@ -1,27 +1,41 @@
 import TerminalANSI
 public import TerminalStyles
 
+/// Spinner is an indeterminate progress indicator.
+///
+/// Spinner fires off strings via an `AsyncStream` at a constant rate until its cancelled. There are a few different
+/// ready-made variations or you can define your own.
 public struct Spinner: Sendable {
+    /// Frames of the spinner.
+    ///
+    /// This is a list of strings that is looped through as long as the spinner runs.
     public var frames: [String]
+
+    /// Frequency of updates to the stream.
     public var frequency: Duration
 
+    /// `dot` is a spinner consisting of characters `⣾`, `⣽`, `⣻`, `⢿`, `⡿`, `⣟`, `⣯`, and `⣷`.
     public static let dot = Spinner(
         frames: ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"],
         frequency: .milliseconds(100),
     )
 
+    /// `jump` is a spinner consisting of characters `⢄`, `⢂`, `⢁`, `⡁`, `⡈`, `⡐`, and `⡠`.
     public static let jump = Spinner(
         frames: ["⢄", "⢂", "⢁", "⡁", "⡈", "⡐", "⡠"],
         frequency: .milliseconds(100),
     )
 
+    /// `line` is a spinner consisting of characters `|`, `/`, `-`, and `\`.
     public static let line = Spinner(frames: ["|", "/", "–", #"\"#], frequency: .milliseconds(100))
 
+    /// `miniDot` is a spinner consisting of characters `⠋`, `⠙`, `⠹`, `⠸`, `⠼`, `⠴`, `⠦`, `⠧`, `⠇`, and `⠏`.
     public static let miniDot = Spinner(
         frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"],
         frequency: .milliseconds(100),
     )
 
+    /// `points` creates a spinner where an "on" point moves through a sequence of "off" points.
     public static func points(length: Int, off: String = "∙", on: String = "●") -> Spinner {
         precondition(length > 0)
         let empty = String(repeating: off, count: length)
@@ -35,15 +49,21 @@ public struct Spinner: Sendable {
         )
     }
 
+    /// `miniDot` is a spinner consisting of characters `█`, `▓`, `▒`, `░`, `▒`, and `▓`.
     public static let pulse = Spinner(
         frames: ["█", "▓", "▒", "░", "▒", "▓"],
         frequency: .milliseconds(200),
     )
 
+    /// `reverse` reverses the spinner.
+    ///
+    /// - SeeAlso: ``reversed()``
     public mutating func reverse() {
         self.frames.reverse()
     }
 
+    /// `reversed` returns the spinner reversed.
+    /// - SeeAlso: ``reverse()``
     public mutating func reversed() -> Spinner {
         var s = self
         s.reverse()
@@ -52,6 +72,7 @@ public struct Spinner: Sendable {
 }
 
 extension Spinner {
+    /// Run the spinner, applying `style` to the generated strings.
     public func run(style: Style) -> AsyncStream<String> {
         let (stream, continuation) = AsyncStream<String>.makeStream()
         let task = Task {
